4a92edd5956ee579ec8d9a95ef3df8b48a103c4b
RISHIKESH KALME <rishikeshkalme111@gmail.com>
2024-12-15 22:35:05 +0530

    cors

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-var cors = require('cors')
+const cors = require('cors');
 
 dotenv.config();  
+
 require('./config/dbConfig');
+
 const app = express();
-app.use(cors());
 
-app.use(express.json());
+app.use(cors()); 
+app.use(express.json()); 
 
-const adduserRoute = require('./views/usersView')
-const superUserRoute = require('./views/superUserView')
+const adduserRoute = require('./views/usersView');
+const superUserRoute = require('./views/superUserView');
 
 const port = parseInt(process.env.PORT);
 
@@ -18,17 +20,26 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 
-app.use('/', adduserRoute);
-app.use('/', superUserRoute);
+app.use('/users', adduserRoute); 
+app.use('/superuser', superUserRoute); 
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Endpoint not found' });
+});
 
-/// server
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal server error' });
+});
 
-app.listen(port,(err)=>{
-
-    if(err){
-        console.log("server not connected", err)
-    }else{
-        console.log(`server running at ${port}`)
+app.listen(port, (err) => {
+    if (err) {
+        console.error("Server failed to start:", err);
+    } else {
+        console.log(`Server running`);
     }
-})
+});
+
+
+
+
